707a82cc8af2e805687c745a7c9e5dda6c0ec709
diya <diyamitradm0103@gmail.com>
2026-09-12 19:22:49 +0530

    Completed the exercises

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
@@ -8,8 +8,14 @@
 */
 
 // Add your code right below, good luck!
-
-
+const firstName="Carlos"
+const lastName="Stevenson"
+const thisYear=1965
+const birthYear=1947
+const fullName= "Carlos Stevenson"
+const age=18
+let greeting="Hello! My name is "+firstName+" "+lastName+" and I am "+(thisYear-birthYear)+" years old."
+greeting="Hello! My name is "+fullName+" and I am "+age+" years old."
 
 
 
