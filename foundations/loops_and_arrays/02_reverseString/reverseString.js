1d21cc0ef5ec1225728f1a5c8daf34a5de31dc38
diya <diyamitradm0103@gmail.com>
2026-09-20 19:48:35 +0530

    Complete 3 loops and arrays exercises

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+let reverse="";
+for(let i=str.length-1;i>=0;i--)
+{
+    let ch=str.charAt(i);
+    reverse+=ch;
+}
+return reverse;
 
 };
 
--- foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
+++ foundations/loops_and_arrays/03_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...itemsToRemove) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!itemsToRemove.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
